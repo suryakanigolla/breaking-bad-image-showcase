bc84334b910d3da6308e8dbac6e19a842848b1b5
Surya Kanigolla <suryakanigolla@gmail.com>
2021-04-04 11:17:15 +0530

    added layout and styles for it

---
+++ src/layouts/IndexLayout.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./IndexLayout.scss";
+
+const IndexLayout = ({children}) => {
+    return(
+        <div className="IndexLayout">
+            {children}
+        </div>
+    );
+} 
+
+export default IndexLayout;
